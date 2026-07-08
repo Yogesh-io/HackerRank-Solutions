8c3317d02b304dfcf5e2eff75b724a4d82494faf
Yogesh-io <connect.yogesh.cse@gmail.com>
2026-07-08 17:54:26 +0530

    removing file

--- python/Strings/string_split_&_join.py
+++
@@ -1,8 +0,0 @@
-def split_and_join(line):
-    words = line.split()
-    return "-".join(words)
-
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
