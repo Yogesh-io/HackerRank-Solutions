11b0bb29a7145a3d362be70190e53076fa81f0e2
Yogesh-io <connect.yogesh.cse@gmail.com>
2026-07-08 17:29:52 +0530

    Python-Nested-Lists

---
+++ python/Basic_data_types/Nested_list.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    students = []
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+    
+    scores = sorted(list(set([student[1] for student in students])))
+    second_lowest_score = scores[1]
+    
+    second_lowest_students = [student[0] for student in students if student[1] == second_lowest_score]
+    
+    second_lowest_students.sort()
+
+    for name in second_lowest_students:
+        print(name)
